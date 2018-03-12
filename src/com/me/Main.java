179ecabf26d16fd14b28f863dfd852268543579a
patrickatcw <pj8392@yahoo.com>
2018-03-12 13:24:44 -0400

    set up initial structure and updated gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,19 @@
+package com.me;
+
+/*directions
+- create a program that implements a simple mobile phone with the following
+capabilities;
+- able to store, modify, remove and query contact names
+- create a master class, MobilePhone, that holds the ArrayList of contacts
+- the MobilePhone class has the functionality listed above
+- add a menu of options that are available
+- options; quit, print list of contacts, add new contacts, update existing contact,
+remove contact and search / find contact
+- when adding or updating be sure to check if the contact already exists (use name)
+- be sure not to expose the inner workings of the arraylist to mobilephone;
+9no ints, no .get(i) etc.
+- mobilephone should do everything with contacts objects only
+ */
+
+public class Main {
+}
