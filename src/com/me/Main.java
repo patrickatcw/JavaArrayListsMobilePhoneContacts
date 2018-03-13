5a6033adbf48071ccd9d65375b72e877752e5c56
patrickatcw <pj8392@yahoo.com>
2018-03-13 09:28:16 -0400

    added while and switch need to create methods of these next

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -23,5 +23,54 @@ public class Main {
     //step 18 implement scanner
     private static Scanner scanner = new Scanner(System.in);
 
+    //step 19 instance for mobilephone class
+    private static MobilePhone mobilePhone = new MobilePhone("440-999-0001");
+
+    public static void main(String[] args) {
+
+        //step 20 while and switch
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while (!quit) {             //step 23 now work on while loop
+            System.out.println("\nEnter action: (6 to show available actions)");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+                case 0:
+                    System.out.println("\nShutting down...");
+
+                case 1:
+                    mobilePhone.printContacts();    //need to create a method for this, step 24 mobilephone class
+                    break;
+
+                case 2:
+                    addNewContact();   //need to create a method for this, step 25 below
+                    break;
+
+                case 3:
+                    updateContact();    //need to create a method for this, step 26 below
+                    break;
+
+                case 4:
+                    removeContact();     //need to create a method for this, step 28 below
+                    break;
+
+                case 5:
+                    queryContact();     //need to create a method for this, step 29 below
+                    break;
+
+                case 6:
+                    printActions();     //need to create a method for this, step 22 below
+                    break;
+
+            }
+
+
+
+
+        }
+
 
 }
